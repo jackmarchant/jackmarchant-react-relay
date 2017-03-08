801ea56a7b4ddcd44f4d693867be03be1dd74642
Jack Marchant <jack@expert360.com>
2017-03-08 22:34:15 +1100

    Added blog posts and custom layout

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,14 @@
 class PostsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound do |error|
+    render :text => 'not found'
+  end
+
   def index
     @posts = Post.all.order('created_at DESC')
   end
 
   def show
+    # post_title = params[:id].gsub('-', ' ')
     @post = Post.find(params[:id])
     @posts = Post.order('created_at DESC').limit(4).offset(1)
   end
