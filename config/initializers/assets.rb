46e773e6a97f56ee568be3ad2585c6108aac9e42
Jack Marchant <jack@expert360.com>
2017-03-10 08:06:39 +1100

    bump version

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,11 +1,11 @@
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
-Rails.application.config.assets.version = '2.1'
+Rails.application.config.assets.version = '2.2'
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
-Rails.application.config.serve_static_assets = false
+# Rails.application.config.serve_static_assets = false
 Rails.application.config.serve_static_files = true
 Rails.application.config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
 Rails.application.config.assets.compile = true
