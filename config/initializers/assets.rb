ad1c0b4f47a40a5293c1ac4a303268f1b77cad3d
Jack Marchant <jack@expert360.com>
2017-03-09 22:31:12 +1100

    try again

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,10 +1,15 @@
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
-Rails.application.config.assets.version = '2.0'
+Rails.application.config.assets.version = '2.1'
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
+Rails.application.config.serve_static_assets = true
+Rails.application.config.serve_static_files = true
+Rails.application.config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
+Rails.application.config.assets.compile = true
+Rails.application.config.assets.digest = true
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
