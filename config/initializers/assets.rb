4f7549f7b4b9f61ce5c942883507a4d9bbc3780a
Jack Marchant <jack@expert360.com>
2017-03-10 08:02:01 +1100

    do not serve static assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,7 +5,7 @@ Rails.application.config.assets.version = '2.1'
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
-Rails.application.config.serve_static_assets = true
+Rails.application.config.serve_static_assets = false
 Rails.application.config.serve_static_files = true
 Rails.application.config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
 Rails.application.config.assets.compile = true
