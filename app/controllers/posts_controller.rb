95ea17e228f76853d8d83295aa0242c4814d74d6
Jack Marchant <jack@expert360.com>
2017-03-11 12:37:05 +1100

    updated_at desc posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   end
 
   def index
-    @posts = Post.all.order('created_at DESC')
+    @posts = Post.all.order('updated_at DESC')
   end
 
   def show
