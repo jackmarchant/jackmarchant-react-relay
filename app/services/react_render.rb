06110b039de1af9aa8931f2b58a870ea44950386
Jack Marchant <jack@expert360.com>
2017-04-09 15:41:37 +1000

    Fix ssl by detecting scheme

--- app/services/react_render.rb
+++ app/services/react_render.rb
@@ -9,6 +9,7 @@ class ReactRender
 
   def perform
     http = Net::HTTP.new(@uri.host, @uri.port)
+    http.use_ssl = @uri.scheme == "https"
     request = Net::HTTP::Post.new(@uri.path)
     request["Content-Type"] = 'application/json'
     request.body = @data.to_json
